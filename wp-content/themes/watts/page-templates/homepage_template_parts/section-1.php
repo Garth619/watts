434b5f249e02406644d550d9e10c615e056ff5a2
Garrett Cullen <garrettwcullen@gmail.com>
2018-10-30 11:17:05 -0700

    push

--- wp-content/themes/watts/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/watts/page-templates/homepage_template_parts/section-1.php
@@ -27,8 +27,50 @@
 	
 	<div class="sec_one_right">
 		
+		<div class="free_consult_bar">
+			
+			<span>Free Consultation</span>
+			
+			<a href="tel:(800) 294-0055">(800) 294-0055</a>
+			
+		</div><!-- free_consult_bar -->
 		
-		
+		<div class="sec_one_slide_wrapper">
+				
+			<div class="sec_one_slider">
+					
+				<div class="sec_one_single_slide">
+						
+					<img class="results_svg" src="<?php bloginfo('template_directory');?>/images/pa-07.svg"/><!-- results_svg -->
+					
+					<span class="amount">$00,000,000</span><!-- amount -->
+					
+					<span class="case_type">AGRICULTURAL Loss</span><!-- case_type -->
+					
+					<span class="info_title">Fees</span><!-- info_title -->
+					<span class="info_amount">$00,000,000</span><!-- info_amount -->
+					
+					<span class="info_title">Fees</span><!-- info_title -->
+					<span class="info_amount">$00,000,000</span><!-- info_amount -->
+					
+					<span class="info_title">Fees</span><!-- info_title -->
+					<span class="info_amount">$00,000,000</span><!-- info_amount -->
+						
+				</div><!-- sec_one_single_slide -->
+					
+			</div><!-- sec_one_slider -->
+				
+			<div class="sec_one_buttons">
+					
+				<div class="sec_one_button_left sec_one_button"></div><!-- sec_one_button_left -->
+					
+				<div class="sec_one_button_right sec_one_button"></div><!-- sec_one_button_right -->
+					
+			</div><!-- sec_one_buttons -->
+				
+		</div><!-- sec_one_slide_wrapper -->
+			
+	
 	</div><!-- sec_one_right -->
 
 	
