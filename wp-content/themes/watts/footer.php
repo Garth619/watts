2bc9650c4cf060e72e2bf8580697da9e85c822df
Garrett Cullen <garrettwcullen@gmail.com>
2018-10-31 11:17:46 -0700

    push

--- wp-content/themes/watts/footer.php
+++ wp-content/themes/watts/footer.php
@@ -17,6 +17,104 @@
 	
 </div><!-- form_wrapper -->
 
+<div class="locations_wrapper">
+	
+	<div class="locations_inner">
+		
+		<img class="footer_logo" src="<?php bloginfo('template_directory');?>/images/logo_ICO.svg"/>
+		
+		<div class="location_columns">
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="loc_col">
+				
+				<a class="loc_phone" href="tel:(800) 294-0055">(800) 294-0055</a><!-- loc_phone -->
+				
+			</div><!-- loc_col -->
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="single_location loc_col">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bldg 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+				
+			</div><!-- loc_col -->
+			
+			<div class="loc_col copyright_col">
+				
+				<span class="copyright_title">©2018 Watts Guerra LLP . All rights reserved.</span>
+
+				<span class="copyright_disclaimer">Mikal Watts & Frank Guerra are the attorneys responsible for the content of this website. Our attorneys are all licensed to practice in the State of Texas. Mikal Watts, Francisco Guerra IV, and Jose G. "Joey" Gonzalez, Jr. are Board Certified in Personal Injury Trial Law by the Texas Board of Legal Specialization. Michael J. Murray are Board Certified in Civil Appellate Law by the Texas Board of Legal Specialization. Unless otherwise indicated, attorneys are not board certified. Principal office is located in San Antonio, Texas.</span>
+				
+			</div><!-- loc_col -->
+			
+		</div><!-- location_columns -->
+		
+	</div><!-- locations_inner -->
+	
+</div><!-- locations_wrapper -->
+
 </footer>
 
 
