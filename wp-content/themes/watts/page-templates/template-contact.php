96b9116430cb609ced986c567ec79b2b9434b6ca
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-01 15:32:57 -0700

    push

--- wp-content/themes/watts/page-templates/template-contact.php
+++ wp-content/themes/watts/page-templates/template-contact.php
@@ -5,6 +5,18 @@
 	get_header(); ?>
 
 
+<div id="internal_main" class="one_col">
 
-			
+<h1 class="centered"><?php the_title();?></h1>
+	
+<div class="internal_container contact_wrapper">
+	
+	
+
+</div><!-- internal_container -->
+
+
+</div><!-- internal_main -->
+
+
 <?php get_footer(); ?>
