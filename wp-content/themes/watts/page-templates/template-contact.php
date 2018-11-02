39d531275bdf8135c52b2c1d718b792ff769f2cf
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-02 12:43:33 -0700

    push

--- wp-content/themes/watts/page-templates/template-contact.php
+++ wp-content/themes/watts/page-templates/template-contact.php
@@ -12,6 +12,118 @@
 <div class="internal_container contact_wrapper">
 	
 	
+	
+	<div class="iframe_map">
+		
+		<iframe src="https://snazzymaps.com/embed/110351" width="100%" height="100%" style="border:none;"></iframe>
+		
+	</div><!-- iframe_map -->
+	
+	<div class="contact_bottom">
+		
+		<div class="contact_left">
+			
+			<div class="logo_consult_wrapper">
+			
+				<img src="<?php bloginfo('template_directory');?>/images/logo_ICO.svg"/>
+			
+				<div class="contact_consult_wrapper">
+				
+					<span>Free Consultation</span>
+				
+					<a href="">(800) 294-0055</a>
+					
+				</div><!-- contact_consult_wrapper -->
+			
+			</div><!-- logo_consult_wrapper -->
+			
+			<div class="contact_left_content content">
+				
+				<p>Our team of lawyers will aggressively pursue your case on a contingency fee basis. This means that you do not have to pay anything up front, and that you are not responsible for expenses we incur unless we are successful with your case. We handle contingency fee cases across the country.</p>
+
+				<p>We have the resources, the experience and the proven track record to help you get the maximum compensation for your case. We are here to help. Contact the law firm of Watts Guerra today.</p>
+
+				
+			</div><!-- contact_left_content -->
+			
+		</div><!-- contact_left -->
+		
+		<div class="contact_right">
+			
+			<div class="single_location">
+				
+				<span class="location_title">San Antonio</span><!-- location_title -->
+				
+				<span class="address">4 Dominion Dr.<br/> Bld 3, Suite 100<br/> San Antonio, TX 78257</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			<div class="single_location">
+				
+				<span class="location_title">Brownsville</span><!-- location_title -->
+				
+				<span class="address">2406 Thor’s Hammer Blvd<br/> Suite 200<br/> Brownsville, TX 78256</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			<div class="single_location">
+				
+				<span class="location_title">Brownsville</span><!-- location_title -->
+				
+				<span class="address">2406 Thor’s Hammer Blvd<br/> Suite 200<br/> Brownsville, TX 78256</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			<div class="single_location">
+				
+				<span class="location_title">Brownsville</span><!-- location_title -->
+				
+				<span class="address">2406 Thor’s Hammer Blvd<br/> Suite 200<br/> Brownsville, TX 78256</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			<div class="single_location">
+				
+				<span class="location_title">Brownsville</span><!-- location_title -->
+				
+				<span class="address">2406 Thor’s Hammer Blvd<br/> Suite 200<br/> Brownsville, TX 78256</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			<div class="single_location">
+				
+				<span class="location_title">Brownsville</span><!-- location_title -->
+				
+				<span class="address">2406 Thor’s Hammer Blvd<br/> Suite 200<br/> Brownsville, TX 78256</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			<div class="single_location">
+				
+				<span class="location_title">Brownsville</span><!-- location_title -->
+				
+				<span class="address">2406 Thor’s Hammer Blvd<br/> Suite 200<br/> Brownsville, TX 78256</span><!-- address -->
+				
+				<a class="directions" href="" target="_blank" rel="noopener">Directions</a>
+
+			</div><!-- single_location -->
+			
+			
+		</div><!-- contact_right -->
+		
+	</div><!-- contact_bottom -->
 
 </div><!-- internal_container -->
 
