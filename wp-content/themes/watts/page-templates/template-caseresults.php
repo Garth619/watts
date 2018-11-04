c06d74acdf46189d7029fac785ce33218f2d64aa
Garrett Cullen <Garth619@users.noreply.github.com>
2018-11-03 19:48:50 -0700

    push

--- wp-content/themes/watts/page-templates/template-caseresults.php
+++ wp-content/themes/watts/page-templates/template-caseresults.php
@@ -14,7 +14,10 @@ get_header(); ?>
 			
 		</div><!-- internal_banner_inner -->
 	
-		<img src="<?php bloginfo('template_directory');?>/images/hero-intl-result.jpg"/>
+		<?php $case_results_banner = get_field( 'case_results_banner' ); ?>
+		
+		<img src="<?php echo $case_results_banner['url']; ?>" alt="<?php echo $case_results_banner['alt']; ?>" />
+
 	
 	</div><!-- internal_banner -->
 
@@ -23,181 +26,72 @@ get_header(); ?>
 
 <div class="internal_container case_results_wrapper">
 	
-	<div class="single_case_results">
-		
-		<div class="single_case_results_inner">
-		
-		<img class="cr_svg" src="<?php bloginfo('template_directory');?>/images/pa-08.svg"/>
-		
-		<span class="cr_single_title">$19,900,000 verdict for pipeline explosion & burn injuries</span><!-- cr_single_title -->
-		
-		<div class="cr_stats">
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Fees</span><!-- stat_title -->
-				
-				<span class="stat_amount">$1,200,000</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Expenses</span><!-- stat_title -->
-				
-				<span class="stat_amount">$77,674.91</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Net to Client</span><!-- stat_title -->
-				
-				<span class="stat_amount">$3,500,00</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-		</div><!-- cr_stats -->
-		
-		<div class="cr_single_content content">
-			
-			<p>Watts Guerra LLP client, Luis Moreno, suffered burns over 57% of his body as the result of pipeline explosion in June 2008.  Last week, after a three week trial by WG lawyers Mikal Watts, John Ramsey and Cesar Perez, a jury in Harris County, Texas awarded Mr. Moreno and two other plaintiffs approximately 19.9 million dollars for the damages suffered as a result of the Vanderbilt pipeline explosion.</p>
-			
-			<a class="cr_read_more">read more of the story as reported Victoria Advocate</a>
-			
-			<div class="read_more_cr_content">
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-			</div><!-- read_more_content -->
-		
-		</div><!-- cr_single_content -->
-		
-		</div><!-- single_case_results_inner -->
 	
-	</div><!-- single_case_results -->
+	<?php if(get_field('case_results_page')): ?>
+	 
+		<?php while(has_sub_field('case_results_page')): ?>
+	 
+			<div class="single_case_results">
+		
+				<div class="single_case_results_inner">
+					
+					<?php $svg = get_sub_field( 'svg' ); ?>
+		
+					<img class="cr_svg" src="<?php echo $svg['url']; ?>" alt="<?php echo $svg['alt']; ?>" />
+		
+					<span class="cr_single_title"><?php the_sub_field( 'case_results_description' ); ?></span><!-- cr_single_title -->
+		
+					<div class="cr_stats">
+			
+						<div class="single_stat">
+				
+							<span class="stat_title"><?php the_sub_field( 'fees_title' ); ?></span><!-- stat_title -->
+				
+							<span class="stat_amount"><?php the_sub_field( 'fees_amount' ); ?></span><!-- stat_amount -->
+				
+						</div><!-- single_stat -->
+			
+						<div class="single_stat">
+				
+							<span class="stat_title"><?php the_sub_field( 'expenses_title' ); ?></span><!-- stat_title -->
+				
+							<span class="stat_amount"><?php the_sub_field( 'expenses_amount' ); ?></span><!-- stat_amount -->
+				
+						</div><!-- single_stat -->
+			
+						<div class="single_stat">
+				
+							<span class="stat_title"><?php the_sub_field( 'net_to_client_title' ); ?></span><!-- stat_title -->
+				
+							<span class="stat_amount"><?php the_sub_field( 'net_to_client_amount' ); ?></span><!-- stat_amount -->
+				
+						</div><!-- single_stat -->
+			
+					</div><!-- cr_stats -->
+		
+					<div class="cr_single_content content">
+			
+						<?php the_sub_field( 'case_results_intro' ); ?>
+			
+						<a class="cr_read_more"><?php the_sub_field( 'case_results_read_more_verboage' ); ?></a>
+			
+						<div class="read_more_cr_content">
+				
+							<?php the_sub_field( 'case_results_additional_content' ); ?>
+				
+					</div><!-- read_more_content -->
+		
+				</div><!-- cr_single_content -->
+		
+			</div><!-- single_case_results_inner -->
 	
-	<div class="single_case_results">
-		
-		<div class="single_case_results_inner">
-		
-		<img class="cr_svg" src="<?php bloginfo('template_directory');?>/images/pa-08.svg"/>
-		
-		<span class="cr_single_title">$19,900,000 verdict for pipeline explosion & burn injuries</span><!-- cr_single_title -->
-		
-		<div class="cr_stats">
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Fees</span><!-- stat_title -->
-				
-				<span class="stat_amount">$1,200,000</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Expenses</span><!-- stat_title -->
-				
-				<span class="stat_amount">$77,674.91</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Net to Client</span><!-- stat_title -->
-				
-				<span class="stat_amount">$3,500,00</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-		</div><!-- cr_stats -->
-		
-		<div class="cr_single_content content">
-			
-			<p>Watts Guerra LLP client, Luis Moreno, suffered burns over 57% of his body as the result of pipeline explosion in June 2008.  Last week, after a three week trial by WG lawyers Mikal Watts, John Ramsey and Cesar Perez, a jury in Harris County, Texas awarded Mr. Moreno and two other plaintiffs approximately 19.9 million dollars for the damages suffered as a result of the Vanderbilt pipeline explosion.</p>
-			
-			<a class="cr_read_more">read more of the story as reported Victoria Advocate</a>
-			
-			<div class="read_more_cr_content">
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-			</div><!-- read_more_content -->
-		
-		</div><!-- cr_single_content -->
-		
-		</div><!-- single_case_results_inner -->
+		</div><!-- single_case_results -->
+	    
+		<?php endwhile; ?>
+	 
+	<?php endif; ?>
 	
-	</div><!-- single_case_results -->
-	
-	<div class="single_case_results">
 		
-		<div class="single_case_results_inner">
-		
-		<img class="cr_svg" src="<?php bloginfo('template_directory');?>/images/pa-08.svg"/>
-		
-		<span class="cr_single_title">$19,900,000 verdict for pipeline explosion & burn injuries</span><!-- cr_single_title -->
-		
-		<div class="cr_stats">
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Fees</span><!-- stat_title -->
-				
-				<span class="stat_amount">$1,200,000</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Expenses</span><!-- stat_title -->
-				
-				<span class="stat_amount">$77,674.91</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-			<div class="single_stat">
-				
-				<span class="stat_title">Net to Client</span><!-- stat_title -->
-				
-				<span class="stat_amount">$3,500,00</span><!-- stat_amount -->
-				
-			</div><!-- single_stat -->
-			
-		</div><!-- cr_stats -->
-		
-		<div class="cr_single_content content">
-			
-			<p>Watts Guerra LLP client, Luis Moreno, suffered burns over 57% of his body as the result of pipeline explosion in June 2008.  Last week, after a three week trial by WG lawyers Mikal Watts, John Ramsey and Cesar Perez, a jury in Harris County, Texas awarded Mr. Moreno and two other plaintiffs approximately 19.9 million dollars for the damages suffered as a result of the Vanderbilt pipeline explosion.</p>
-			
-			<a class="cr_read_more">read more of the story as reported Victoria Advocate</a>
-			
-			<div class="read_more_cr_content">
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-			</div><!-- read_more_content -->
-		
-		</div><!-- cr_single_content -->
-		
-		</div><!-- single_case_results_inner -->
-	
-	</div><!-- single_case_results -->
-
-	
 </div><!-- internal_container -->
 
 
