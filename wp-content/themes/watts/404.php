46cb40a2006915b67d7c5f0a9f0aa46a0678352b
Garrett Cullen <Garth619@users.noreply.github.com>
2018-11-03 21:25:49 -0700

    push

--- wp-content/themes/watts/404.php
+++ wp-content/themes/watts/404.php
@@ -1,17 +1,40 @@
 <?php get_header(); ?>
 
 
-<span onclick="goBack()">go back</span>
-	
-<!--
-	<script type="text/javascript">
 
-		function goBack() {
-    	window.history.back();
-		}
 
-	</script>
--->
+<div id="internal_main" class="one_col">
+
+
+
+<div class="internal_container content">
 	
+	<h1 class="centered">Not Found</h1>
+	
+	<p>This page is not found, please return <a href="<?php bloginfo('url');?>">home</a> or <span class="go_back" onclick="goBack()">go back</span></p>
+
+	
+			
+</div><!-- internal_container -->
+
+
+</div><!-- internal_main -->
+
+
+<script type="text/javascript">
+
+	function goBack() {
+    window.history.back();
+	}
+
+</script>
+
 
 <?php get_footer(); ?>
+
+
+
+
+	
+	
+
