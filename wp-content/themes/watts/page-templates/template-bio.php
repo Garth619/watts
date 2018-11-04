59b212266346f36acaf6d48bc2f570cec5a087cc
Garrett Cullen <Garth619@users.noreply.github.com>
2018-11-04 10:14:06 -0800

    push

--- wp-content/themes/watts/page-templates/template-bio.php
+++ wp-content/themes/watts/page-templates/template-bio.php
@@ -5,8 +5,64 @@
 get_header(); ?>
 
 
+<div id="internal_main" class="one_col">
+	
+	<h1 class="centered"><?php the_title();?></h1>
 
-	<?php get_template_part( 'loop', 'page' ); ?>
+	<div class="internal_container bio_wrapper">
+		
+		<div class="att_bio_intro">
+			
+			<div class="bio_blueline_wrapper">
+				
+				<span class="bio_blueline"></span><!-- bio_blueline -->
+				
+			</div><!-- bio_blueline_wrapper -->
+			
+			<div class="att_bio_intro_left">
+				
+				<div class="att_bio_video">
+					
+					<a class="" href="">
+						
+						<div class="bio_img_wrapper">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+						
+						</div><!-- bio_img_wrapper -->
+						
+						<div class="bio_video_title_wrapper">
+							
+							<div class="bio_play_wrapper"></div><!-- bio_play_wrapper -->
+							
+							<span class="bio_video_title">Hear from the Attorney</span><!-- bio_video_title -->
+							
+						</div><!-- bio_video_title_wrapper -->
+					
+					</a>
+					
+				</div><!-- att_bio_video -->
+				
+			</div><!-- att_bio_intro_left -->
+			
+			<div class="att_bio_intro_right content">
+				
+				<p>Born July 17, 1967, in Corpus Christi, Texas, Mikal C. Watts earned his undergraduate degree from the University of Texas in 1987, receiving a Bachelor of Arts with high honors after just two years of study. He graduated with honors from the University of Texas School of Law in 1989 at the age of twenty-one.</p>
+
+				<p>In 1989-1990, Mr. Watts worked as a briefing attorney for Hon. Thomas R. Phillips, Chief Justice of the Supreme Court of Texas. In September, 1990, he became an associate at David L. Perry & Associates in Corpus Christi, Texas, and was named a partner in December, 1991. Until March 31, 1997, he served as a partner in the law firm of Perry & Haas, L.L.P. On April 1, 1997, Mr. Watts formed his own firm, Harris & Watts, P.C. After 4 highly successful years, the law firm reorganized, with Mr. Watts and Denman H. Heard joining together to form Watts & Heard, L.L.P. in March of 2001. During that time, Mr. Watts expanded the law firm to 29 lawyers, with offices in 5 Texas cities and a support staff of over 100. In August of 2002, Mr. Watts formed Watts Law Firm, L.L.P. In 2009, Mikal Watts joined forces with Francisco Guerra IV to form Watts Guerra LLP which handles catastrophic personal injury, toxic torts, product liability, automotive defects, refinery negligence, commercial trucking negligence, medical device, pharmaceutical, and commercial litigation.</p>
+				
+			</div><!-- att_bio_intro_right -->
+			
+		</div><!-- att_bio_intro -->
+	
+	</div><!-- internal_container -->
+
+</div><!-- internal_main -->
+
+
+<?php get_footer(); ?>
+
+	<?php // get_template_part( 'loop', 'page' ); ?>
 					 
 					 	
-<?php get_footer(); ?>
+
