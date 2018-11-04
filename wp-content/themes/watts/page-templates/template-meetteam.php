c8e6069f722d461ef926f3c819a87e0be1999e57
Garrett Cullen <Garth619@users.noreply.github.com>
2018-11-04 13:51:00 -0800

    push

--- wp-content/themes/watts/page-templates/template-meetteam.php
+++ wp-content/themes/watts/page-templates/template-meetteam.php
@@ -4,7 +4,328 @@
 
 get_header(); ?>
 
+<div id="internal_main">
+		
+	<h1 class="centered"><?php the_title();?></h1>
+	
+	<div class="att_attorneys">
+		
+		<div class="single_attorney">
+			
+			<a class="" href="">
+				
+				<div class="attorney_image">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+					<div class="img_overlay"></div><!-- img_overlay -->
+					
+				</div><!-- attorney_image -->
+				
+				<div class="attorney_title_wrapper">
+					
+					<span class="att_name">Mikal C. Watts</span><!-- att_name -->
+					
+					<span class="att_position">attorney</span><!-- att_position -->
+					
+				</div><!-- attorney_title_wrapper -->
+				
+			</a>
+			
+		</div><!-- single_attorney -->
+		
+		<div class="single_attorney">
+			
+			<a class="" href="">
+				
+				<div class="attorney_image">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+					<div class="img_overlay"></div><!-- img_overlay -->
+					
+				</div><!-- attorney_image -->
+				
+				<div class="attorney_title_wrapper">
+					
+					<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+					<span class="att_position">attorney</span><!-- att_position -->
+					
+				</div><!-- attorney_title_wrapper -->
+				
+			</a>
+			
+		</div><!-- single_attorney -->
+		
+		
+	</div><!-- att_attorneys -->
 
+	<div class="internal_container meetteam_wrapper">
+		
+		
+		<div class="meetteam_section">
+			
+			<div class="meetteam_title_wrapper">
+			
+			<span class="meetteam_title">Attorneys</span><!-- meetteam_title -->
+			
+			<span class="meeteam_line"></span><!-- meeteam_line -->
+			
+			</div><!-- meetteam_title_wrapper -->
+			
+			<div class="meetteam_members">
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+			</div><!-- meetteam_members -->
+			
+		</div><!-- meetteam_section -->
+		
+		
+		<div class="meetteam_section">
+			
+			<div class="meetteam_title_wrapper">
+			
+			<span class="meetteam_title">Of Counsel</span><!-- meetteam_title -->
+			
+			<span class="meeteam_line"></span><!-- meeteam_line -->
+			
+			</div><!-- meetteam_title_wrapper -->
+			
+			<div class="meetteam_members">
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+				<div class="single_attorney">
+			
+					<a class="" href="">
+				
+						<div class="attorney_image">
+					
+							<img src="<?php bloginfo('template_directory');?>/images/att-bio.jpg"/>
+					
+							<div class="img_overlay"></div><!-- img_overlay -->
+					
+						</div><!-- attorney_image -->
+				
+						<div class="attorney_title_wrapper">
+					
+							<span class="att_name">Francisco Guerra</span><!-- att_name -->
+					
+						</div><!-- attorney_title_wrapper -->
+				
+					</a>
+			
+				</div><!-- single_attorney -->
+				
+			</div><!-- meetteam_members -->
+			
+		</div><!-- meetteam_section -->
+		
+	
+	</div><!-- internal_container -->
+
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
+
+
+					 
+					 	
+
