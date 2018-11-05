4da5ebadc725a0cdd957cddc17e58c4a05f44c93
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-05 13:35:33 -0800

    push

--- wp-content/themes/watts/page-templates/template-about.php
+++ wp-content/themes/watts/page-templates/template-about.php
@@ -72,6 +72,8 @@ get_header(); ?>
 					<img src="<?php bloginfo('template_directory');?>/images/pa-03.svg"/>
 					
 					<span class="single_pa_title">Automotive product liability</span><!-- single_pa_title -->
+					
+					<span class="aboutline"></span><!-- aboutline -->
 				
 				</div><!-- single_pa_content -->
 				
@@ -89,15 +91,17 @@ get_header(); ?>
 				
 				<div class="single_pa_content">
 				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-03.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/pa-04.svg"/>
 					
-					<span class="single_pa_title">Automotive product liability</span><!-- single_pa_title -->
+					<span class="single_pa_title">pharmaceutical<br/> mass torts</span><!-- single_pa_title -->
+					
+					<span class="aboutline"></span><!-- aboutline -->
 				
 				</div><!-- single_pa_content -->
 				
 				<div class="single_pa_hover">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/about-car-white.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/pa-04-white.svg"/>
 					
 					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
 					
@@ -109,15 +113,17 @@ get_header(); ?>
 				
 				<div class="single_pa_content">
 				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-03.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/pa-06.svg"/>
 					
-					<span class="single_pa_title">Automotive product liability</span><!-- single_pa_title -->
+					<span class="single_pa_title">dangerous medical devices</span><!-- single_pa_title -->
+					
+					<span class="aboutline"></span><!-- aboutline -->
 				
 				</div><!-- single_pa_content -->
 				
 				<div class="single_pa_hover">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/about-car-white.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/pa-06-white.svg"/>
 					
 					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
 					
@@ -129,15 +135,17 @@ get_header(); ?>
 				
 				<div class="single_pa_content">
 				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-03.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/pa-02.svg"/>
 					
-					<span class="single_pa_title">Automotive product liability</span><!-- single_pa_title -->
+					<span class="single_pa_title">commercial litigation</span><!-- single_pa_title -->
+					
+					<span class="aboutline"></span><!-- aboutline -->
 				
 				</div><!-- single_pa_content -->
 				
 				<div class="single_pa_hover">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/about-car-white.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/pa-02-white.svg"/>
 					
 					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
 					
@@ -146,6 +154,61 @@ get_header(); ?>
 			</div><!-- about_single_pa -->
 			
 		</div><!-- about_pa -->
+		
+		<div class="about_sellingpoints">
+			
+			<div class="about_sp_col">
+				
+				<h2>What Makes Watts Guerra Different?</h2>
+				
+				<div class="single_sp">
+					
+					<span class="single_sp_title">Bigger</span><!-- single_sp_title -->
+					
+					<span class="single_sp_conten content">Our firm employs a team of people in cities across Texas. We boast one of the most experienced plaintiff-side lawyer rosters in the country, with some of the best trial lawyers in their respective areas of practice. We have strength in numbers and in talent.</span><!-- single_sp_content -->
+					
+				</div><!-- single_sp -->
+				
+				<div class="single_sp">
+					
+					<span class="single_sp_title">Better</span><!-- single_sp_title -->
+					
+					<span class="single_sp_conten content">Our results speak for themselves. Our resources are larger than most firms, enabling us to invest tens of millions of dollars at once to battle the largest corporations in the world.  We will go to work for you, employing the best experts, and using the most up-to-date technology available.  For example, we have portable trial trailers with satellite technology and vehicle wreckage travel trailers to collect a damaged car, truck or bus.  </span><!-- single_sp_content -->
+					
+				</div><!-- single_sp -->
+				
+			</div><!-- about_sp_col -->
+			
+			<div class="about_sp_col">
+				
+				<div class="single_sp">
+					
+					<span class="single_sp_title">exceptional results</span><!-- single_sp_title -->
+					
+					<span class="single_sp_conten content">We’ve won groundbreaking trials for individuals catastrophically injured by defective products and drugs.</span><!-- single_sp_content -->
+					
+				</div><!-- single_sp -->
+				
+				<div class="single_sp">
+					
+					<span class="single_sp_title">contingency fees</span><!-- single_sp_title -->
+					
+					<span class="single_sp_conten content">Full-time staff pilots fly private planes on a moment’s notice, enabling our lawyers to quickly meet with clients and travel easily to depositions and mediations across the country.  We will aggressively seek the first trial setting for your case, never wasting time to bring justice to our deserving clients.</span><!-- single_sp_content -->
+					
+				</div><!-- single_sp -->
+				
+				<div class="single_sp">
+					
+					<span class="single_sp_title">faster</span><!-- single_sp_title -->
+					
+					<span class="single_sp_conten content">The lawyers of Watts Guerra have a proven track record of serious, high-value results.  We believe in our ability to win your case; therefore, we accept all of our cases on a contingency fee basis.  You will not owe us anything unless we recover on your behalf</span><!-- single_sp_content -->
+					
+				</div><!-- single_sp -->
+				
+				
+			</div><!-- about_sp_col -->
+			
+		</div><!-- about_sellingpoints -->
 		
 		
 </div><!-- internal_container -->
