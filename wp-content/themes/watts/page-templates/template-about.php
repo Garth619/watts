4335ff6376bae15264bad14132b3c5ad38c4831e
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-05 14:54:33 -0800

    push

--- wp-content/themes/watts/page-templates/template-about.php
+++ wp-content/themes/watts/page-templates/template-about.php
@@ -19,13 +19,11 @@ get_header(); ?>
 				
 				<div class="about_intro">
 				
-					<p><strong>Our lawyers came to Watts Guerra from the best and largest defense firms, companies, prosecutors' offices, and other plantiffs' firms. We're experienced, having tried hundreds of lawsuits and represented hundreds of thousands of people.</strong></p>
+					<?php the_field( 'about_intro' ); ?>
 				
 				</div><!-- about_intro -->
 				
-				<p>Watts Guerra LLP is a true nationwide litigation practice.  Our trial lawyers handle some of the largest cases nationally for catastrophic injury and death, product liability, commercial litigation, and mass torts.</p>
-
-				<p>We’ve beaten the largest and most powerful corporations in America, yielding substantial verdicts and settlements and, more importantly, greater consumer safety for everyone.  For more information, contact us today.</p>
+				<?php the_field( 'about_content' ); ?>
 				
 			</div><!--about_left -->
 			
@@ -33,11 +31,14 @@ get_header(); ?>
 				
 				<div class="about_video">
 					
-					<a class="" href="//player.vimeo.com/video/32001208?portrait=0&autoplay=1" data-lity>
+					<a class="" href="//player.vimeo.com/video/<?php the_field( 'about_vimeo_id' ); ?>?portrait=0&autoplay=1" data-lity>
 					
 						<div class="about_image_overlay">
 						
-							<img src="<?php bloginfo('template_directory');?>/images/video-about.jpg"/>
+							<?php $video_image = get_field( 'video_image' ); ?>
+
+							<img src="<?php echo $video_image['url']; ?>" alt="<?php echo $video_image['alt']; ?>" />
+
 						
 						</div><!-- about_image_overlay -->
 						
@@ -49,7 +50,7 @@ get_header(); ?>
 								
 							</div><!-- play_button_wrapper -->
 							
-							<span class="about_video_title">Learn About Watts Guerra</span><!-- about_video_title -->
+							<span class="about_video_title"><?php the_field( 'video_call_to_action' ); ?></span><!-- about_video_title -->
 							
 						</div><!-- about_video_text -->
 					
@@ -65,145 +66,87 @@ get_header(); ?>
 		
 		<div class="about_pa">
 			
-			<div class="about_single_pa">
-				
-				<div class="single_pa_content">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-03.svg"/>
-					
-					<span class="single_pa_title">Automotive product liability</span><!-- single_pa_title -->
-					
-					<span class="aboutline"></span><!-- aboutline -->
-				
-				</div><!-- single_pa_content -->
-				
-				<div class="single_pa_hover">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/about-car-white.svg"/>
-					
-					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
-					
-				</div><!-- single_pa_hover -->
-				
-			</div><!-- about_single_pa -->
 			
-			<div class="about_single_pa">
+			<?php if(get_field('about_selling_points')): ?>
+			 
+				<?php while(has_sub_field('about_selling_points')): ?>
+			 
+					<div class="about_single_pa">
 				
-				<div class="single_pa_content">
+						<div class="single_pa_content">
 				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-04.svg"/>
+							<?php $svg = get_sub_field( 'svg' ); ?>
+							
+							<img src="<?php echo $svg['url']; ?>" alt="<?php echo $svg['alt']; ?>" />
+							
+							<span class="single_pa_title"><?php the_sub_field( 'selling_point_type' ); ?></span><!-- single_pa_title -->
 					
-					<span class="single_pa_title">pharmaceutical<br/> mass torts</span><!-- single_pa_title -->
-					
-					<span class="aboutline"></span><!-- aboutline -->
+							<span class="aboutline"></span><!-- aboutline -->
 				
-				</div><!-- single_pa_content -->
+						</div><!-- single_pa_content -->
 				
-				<div class="single_pa_hover">
+						<div class="single_pa_hover">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/pa-04-white.svg"/>
+						<?php $svg_hover = get_sub_field( 'svg_hover' ); ?>
+		
+						<img src="<?php echo $svg_hover['url']; ?>" alt="<?php echo $svg_hover['alt']; ?>" />
+		
 					
-					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
+						<span class="single_pa_hover_content"><?php the_sub_field( 'selling_points_content' ); ?></span><!-- single_pa_hover_content -->
 					
-				</div><!-- single_pa_hover -->
+					</div><!-- single_pa_hover -->
 				
-			</div><!-- about_single_pa -->
+				</div><!-- about_single_pa -->
+			    
+				<?php endwhile; ?>
+			 
+			<?php endif; ?>
 			
-			<div class="about_single_pa">
-				
-				<div class="single_pa_content">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-06.svg"/>
 					
-					<span class="single_pa_title">dangerous medical devices</span><!-- single_pa_title -->
-					
-					<span class="aboutline"></span><!-- aboutline -->
-				
-				</div><!-- single_pa_content -->
-				
-				<div class="single_pa_hover">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/pa-06-white.svg"/>
-					
-					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
-					
-				</div><!-- single_pa_hover -->
-				
-			</div><!-- about_single_pa -->
-			
-			<div class="about_single_pa">
-				
-				<div class="single_pa_content">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/pa-02.svg"/>
-					
-					<span class="single_pa_title">commercial litigation</span><!-- single_pa_title -->
-					
-					<span class="aboutline"></span><!-- aboutline -->
-				
-				</div><!-- single_pa_content -->
-				
-				<div class="single_pa_hover">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/pa-02-white.svg"/>
-					
-					<span class="single_pa_hover_content">We were proud to represent pharmaceutical victim Margie Sanchez, whose liver was destroyed by the diabetes drug Rezulin, helping her to receive a significant jury award in the first Rezulin trial in the nation.</span><!-- single_pa_hover_content -->
-					
-				</div><!-- single_pa_hover -->
-				
-			</div><!-- about_single_pa -->
-			
 		</div><!-- about_pa -->
 		
 		<div class="about_sellingpoints">
 			
 			<div class="about_sp_col">
 				
-				<h2>What Makes Watts Guerra Different?</h2>
+				<h2><?php the_field( 'about_bottom_content_header' ); ?></h2>
 				
-				<div class="single_sp">
-					
-					<span class="single_sp_title">Bigger</span><!-- single_sp_title -->
-					
-					<span class="single_sp_conten content">Our firm employs a team of people in cities across Texas. We boast one of the most experienced plaintiff-side lawyer rosters in the country, with some of the best trial lawyers in their respective areas of practice. We have strength in numbers and in talent.</span><!-- single_sp_content -->
-					
-				</div><!-- single_sp -->
 				
-				<div class="single_sp">
+				<?php if(get_field('about_spelling_points_verbiage_col_one')): ?>
+				 
+					<?php while(has_sub_field('about_spelling_points_verbiage_col_one')): ?>
+				 
+						<div class="single_sp">
 					
-					<span class="single_sp_title">Better</span><!-- single_sp_title -->
+							<span class="single_sp_title"><?php the_sub_field( 'selling_point_header' ); ?></span><!-- single_sp_title -->
 					
-					<span class="single_sp_conten content">Our results speak for themselves. Our resources are larger than most firms, enabling us to invest tens of millions of dollars at once to battle the largest corporations in the world.  We will go to work for you, employing the best experts, and using the most up-to-date technology available.  For example, we have portable trial trailers with satellite technology and vehicle wreckage travel trailers to collect a damaged car, truck or bus.  </span><!-- single_sp_content -->
+							<span class="single_sp_conten content"><?php the_sub_field( 'selling_point_verbiage' ); ?></span><!-- single_sp_content -->
 					
-				</div><!-- single_sp -->
-				
+						</div><!-- single_sp -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
+								
 			</div><!-- about_sp_col -->
 			
 			<div class="about_sp_col">
 				
-				<div class="single_sp">
+				<?php if(get_field('about_spelling_points_verbiage_col_two')): ?>
+				 
+					<?php while(has_sub_field('about_spelling_points_verbiage_col_two')): ?>
+				 
+						<div class="single_sp">
 					
-					<span class="single_sp_title">exceptional results</span><!-- single_sp_title -->
+							<span class="single_sp_title"><?php the_sub_field( 'selling_point_header' ); ?></span><!-- single_sp_title -->
 					
-					<span class="single_sp_conten content">We’ve won groundbreaking trials for individuals catastrophically injured by defective products and drugs.</span><!-- single_sp_content -->
+							<span class="single_sp_conten content"><?php the_sub_field( 'selling_point_verbiage' ); ?></span><!-- single_sp_content -->
 					
-				</div><!-- single_sp -->
-				
-				<div class="single_sp">
-					
-					<span class="single_sp_title">contingency fees</span><!-- single_sp_title -->
-					
-					<span class="single_sp_conten content">Full-time staff pilots fly private planes on a moment’s notice, enabling our lawyers to quickly meet with clients and travel easily to depositions and mediations across the country.  We will aggressively seek the first trial setting for your case, never wasting time to bring justice to our deserving clients.</span><!-- single_sp_content -->
-					
-				</div><!-- single_sp -->
-				
-				<div class="single_sp">
-					
-					<span class="single_sp_title">faster</span><!-- single_sp_title -->
-					
-					<span class="single_sp_conten content">The lawyers of Watts Guerra have a proven track record of serious, high-value results.  We believe in our ability to win your case; therefore, we accept all of our cases on a contingency fee basis.  You will not owe us anything unless we recover on your behalf</span><!-- single_sp_content -->
-					
-				</div><!-- single_sp -->
+						</div><!-- single_sp -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 				
 				
 			</div><!-- about_sp_col -->
