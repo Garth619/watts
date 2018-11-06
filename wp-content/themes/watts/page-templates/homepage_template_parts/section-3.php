4128a61444f19d48c4bc0140f9852f1623ec0cd1
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-06 11:09:01 -0800

    push

---
+++ wp-content/themes/watts/page-templates/homepage_template_parts/section-3.php
@@ -0,0 +1,54 @@
+<section id="section_three">
+	
+	
+	<div class="sec_three_inner content">
+		
+		<h1>San antonio personal injury attorneys</h1>
+		
+		<div class="sec_three_top">
+			
+			<div class="sec_three_left">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/img-1.jpg"/>
+				
+			</div><!-- sec_three_left -->
+			
+			<div class="sec_three_right">
+				
+				
+				<h2>Pellentesque tempus lobortis dolor, in blandit metus congue ac</h2>
+				
+				<p>Nam at felis vitae orci dignissim pretium et vel nulla.Morbi id ex interdum, vulputate diam pretium, bibendum lectus. mi, fermentum id interdum eget, ultricies sit amet neque. Aenean efficitur aliquam mauris, in sodales ex tristique id. Curabitur est urna, ultrices ac tempor ac, varius porta velit. Nunc dui ante, suscipit non odio in, tincidunt lobortis ipsum. Vestibulum mollis lorem urna, sed fringilla lectus efficitur sed.</p>
+
+				<p>Curabitur felis ligula, placerat non efficitur vel, ornare nec ligula. Donec molestie elit gravida, consequat quam sed, porttitor arcu. Donec vitae augue in ipsum tincidunt volutpat. Nam at felis vitae orci dignissim pretium et vel nulla. Nam et fringilla sapien, ac ullamcorper nunc. Suspendisse potenti. Nullam id lacinia diam, nec facilisis diam. Pellentesque eget sapien non erat porttitor ullamcorper. Donec maximus sapien eu eleifend viverra. In fringilla faucibus lectus, in efficitur metus suscipit sit amet. Nunc rhoncus nisl non leo lacinia, eget porta ex laoreet. Nulla est turpis, aliquam ut tempus eu, pharetra in leo. Sed ullamcorper felis hendrerit, placerat tellus at, fermentum ex.</p>
+				
+			</div><!-- sec_three_right -->
+			
+		</div><!-- sec_three_top -->
+		
+		<div class="sec_three_middle">
+			
+			<div class="sec_three_middle_inner">
+			
+				<div class="sec_three_middle_left">
+				
+					<span>We will go to work for you, employing the best experts, and using the most up-to-date technology.</span>
+				
+				</div><!-- sec_three_middle_left -->
+			
+				<div class="sec_three_middle_right">
+				
+					<span>Curabitur felis ligula, placerat non efficitur vel, ornare nec ligula. Donec molestie elit gravida, consequat quam sed, porttitor arcu. Donec vitae augue in ipsum tincidunt volutpat. Nam at felis vitae orci dignissim pretium et vel nulla. Morbi id ex interdum, vulputate diam pretium, bibendum lectus. Morbi leo mi, fermentum id interdum eget, ultricies sit amet neque.</span>
+				
+				
+				</div><!-- sec_three_middle_right -->
+			
+			</div><!-- sec_three_middle_inner -->
+			
+		</div><!-- sec_three_middle -->
+		
+		
+	</div><!-- sec_three_inner -->
+	
+	
+</section><!-- section_three -->
--- wp-content/themes/watts/page-templates/template-home.php
+++ wp-content/themes/watts/page-templates/template-home.php
@@ -8,6 +8,7 @@
 
 <?php get_template_part( 'page-templates/homepage_template_parts/section', '1' );?>
 <?php get_template_part( 'page-templates/homepage_template_parts/section', '2' );?>
+<?php get_template_part( 'page-templates/homepage_template_parts/section', '3' );?>
 
 
 
