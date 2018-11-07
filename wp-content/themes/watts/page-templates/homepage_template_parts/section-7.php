bb9cf08c43cc1eeb3d486bc95aaec2625034c72e
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-07 15:19:23 -0800

    push

--- wp-content/themes/watts/page-templates/homepage_template_parts/section-7.php
+++ wp-content/themes/watts/page-templates/homepage_template_parts/section-7.php
@@ -2,7 +2,195 @@
 	
 	<div class="sec_seven_inner">
 		
+		<div class="sec_seven_title_wrapper">
+			
+			<span class="sec_seven_title">Cases We Handle</span><!-- sec_seven_title -->
+			
+			<span class="sec_seven_blueline"></span><!-- sec_seven_blueline -->
+			
+		</div><!-- sec_seven_title_wrapper -->
 		
+		<div class="sec_seven_pa">
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+			<div class="sec_seven_single_pa">
+				
+				<a class="" href="">
+					
+					<div class="sec_seven_reg">
+					
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+					
+						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+					
+					</div><!-- sec_seven_reg -->
+					
+					<div class="sec_seven_hover">
+						
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						
+					</div><!-- sec_seven_hover -->
+					
+				</a>
+				
+			</div><!-- sec_seven_single_pa -->
+			
+		</div><!-- sec_seven_pa -->
+		
+		<a class="button pa_view_all" href="">View All</a>
 		
 	</div><!-- sec_seven_inner -->
 	
