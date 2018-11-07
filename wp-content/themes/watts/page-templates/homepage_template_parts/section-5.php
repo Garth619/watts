e4111a0e99ab54c75d7e1a446f89a8167a5965b7
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-07 09:41:25 -0800

    push

---
+++ wp-content/themes/watts/page-templates/homepage_template_parts/section-5.php
@@ -0,0 +1,128 @@
+<section id="section_five">
+	
+	<div class="sec_five_inner">
+		
+		<div class="sec_five_title_wrapper">
+			
+			<span class="sec_five_title">Client Resources</span><!-- sec_five_title -->
+			
+			<span class="sec_five_line"></span><!-- sec_five_line -->
+			
+		</div><!-- sec_five_title_wrapper -->
+		
+		<div class="sec_five_video_wrapper">
+				
+				<div class="sec_five_single_video">
+					
+					<a href="//player.vimeo.com/video/32001208?portrait=0&autoplay=1" data-lity >
+						
+						<div class="sec_five_img_overlay">
+							
+							<img src="<?php bloginfo('template_directory');?>/images/video-1.jpg"/>
+							
+						</div><!-- sec_five_img_overlay -->
+						
+						<div class="sec_five_video_text">
+							
+							<div class="play_button_wrapper">
+						
+								<div class="play_button"></div><!-- play_button -->
+						
+							</div><!-- play_button_wrapper -->
+							
+							<span>Video Title Here</span>
+							
+						</div><!-- sec_five_video_text -->
+						
+					</a>
+					
+				</div><!-- sec_five_single_video -->
+				
+				
+				<div class="sec_five_single_video">
+					
+					<a href="//player.vimeo.com/video/32001208?portrait=0&autoplay=1" data-lity >
+						
+						<div class="sec_five_img_overlay">
+							
+							<img src="<?php bloginfo('template_directory');?>/images/video-2.jpg"/>
+							
+						</div><!-- sec_five_img_overlay -->
+						
+						<div class="sec_five_video_text">
+							
+							<div class="play_button_wrapper">
+						
+								<div class="play_button"></div><!-- play_button -->
+						
+							</div><!-- play_button_wrapper -->
+							
+							<span>really long video title here that has to go on two lines</span>
+							
+						</div><!-- sec_five_video_text -->
+						
+					</a>
+					
+				</div><!-- sec_five_single_video -->
+				
+				<div class="sec_five_single_video">
+					
+					<a href="//player.vimeo.com/video/32001208?portrait=0&autoplay=1" data-lity >
+						
+						<div class="sec_five_img_overlay">
+							
+							<img src="<?php bloginfo('template_directory');?>/images/video-3.jpg"/>
+							
+						</div><!-- sec_five_img_overlay -->
+						
+						<div class="sec_five_video_text">
+							
+							<div class="play_button_wrapper">
+						
+								<div class="play_button"></div><!-- play_button -->
+						
+							</div><!-- play_button_wrapper -->
+							
+							<span>Video Title Here</span>
+							
+						</div><!-- sec_five_video_text -->
+						
+					</a>
+					
+				</div><!-- sec_five_single_video -->
+				
+				<div class="sec_five_single_video">
+					
+					<a href="//player.vimeo.com/video/32001208?portrait=0&autoplay=1" data-lity >
+						
+						<div class="sec_five_img_overlay">
+							
+							<img src="<?php bloginfo('template_directory');?>/images/video-4.jpg"/>
+							
+						</div><!-- sec_five_img_overlay -->
+						
+						<div class="sec_five_video_text">
+							
+							<div class="play_button_wrapper">
+						
+								<div class="play_button"></div><!-- play_button -->
+						
+							</div><!-- play_button_wrapper -->
+							
+							<span>Video Title Here</span>
+							
+						</div><!-- sec_five_video_text -->
+						
+					</a>
+					
+				</div><!-- sec_five_single_video -->
+				
+
+				
+			</div><!-- video_wrapper -->
+			
+			<a class="button sec_five_learn_more" href="">Learn More</a>
+		
+	</div><!-- sec_five_inner -->
+	
+</section><!-- section_five -->
