06d38987663425fcc52581c12a934424c26bacf7
Garrett Cullen <Garth619@users.noreply.github.com>
2018-11-07 19:52:12 -0800

    push

--- wp-content/themes/watts/page-templates/homepage_template_parts/section-7.php
+++ wp-content/themes/watts/page-templates/homepage_template_parts/section-7.php
@@ -18,7 +18,7 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-01.svg"/>
 					
 						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
 					
@@ -26,7 +26,7 @@
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-01-white.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -40,15 +40,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-02.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Business<br/> Litigation</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-02-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -62,15 +62,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-04.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Pharmaceutical<br/> Mass Torts</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-04-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -84,15 +84,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-03.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Car Accidents</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-03-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -106,15 +106,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-06.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Medical<br/> Malpractice</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-06-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -128,15 +128,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-05.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Truck Accidents</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-05-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -150,15 +150,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-07.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Agriculture Losses</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-07-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
@@ -172,15 +172,15 @@
 					
 					<div class="sec_seven_reg">
 					
-						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-color.svg"/>
+						<img class="grad_svg" src="<?php bloginfo('template_directory');?>/images/pa-08.svg"/>
 					
-						<span class="single_pa_title">Product Liability</span><!-- single_pa_title -->
+						<span class="single_pa_title">Workplace Accidents</span><!-- single_pa_title -->
 					
 					</div><!-- sec_seven_reg -->
 					
 					<div class="sec_seven_hover">
 						
-						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white.svg"/>
+						<img class="white_svg" src="<?php bloginfo('template_directory');?>/images/pa-08-white-01.svg"/>
 						
 					</div><!-- sec_seven_hover -->
 					
